76e6f3b7b91809f5413a8aaba477c0ceb55ae9f0
squeaky1273 <pjriddell22@gmail.com>
2020-01-07 10:19:18 -0800

    added to views

--- forum/settings.py
+++ forum/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'question',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+# playlist app settings
+QUESTION_NAME_MAX_LENGTH = 600
+
+
+DEFAULT_LOGOUT_URL = '/'
+# Where to redirect during authentication
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
--- question/views.py
+++ question/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.shortcuts import render, HttpResponseRedirect
+from django.views.generic import CreateView
 
-# Create your views here.
+from question.models import Question, Comment
+from question.forms import QuestionForm, CommentForm
+
+
+class QuestionListView(ListView):
+    """ Renders a list of all Question. """
+    model = Question
+
+    def get(self, request):
+        """ GET a list of Questions. """
+        questions = self.get_queryset().all()
+        return render(request, 'index.html', {
+          'questions': questions
+        })
+
+class QuestionDetailView(DetailView):
+    """ Renders a specific question based on it's slug."""
+    model = Question
+
+    def get(self, request, slug):
+        """ Returns a specific question by slug. """
+        question = self.get_queryset().get(slug__iexact=slug)
+        comment_form = CommentForm()
+        return render(request, 'question.html', {
+          'question': question,
+          'comment_form': comment_form
+        })
+
+    def post(self, request, slug):
+        form = CommentForm(request.POST)
+        if form.is_valid():
+          post = form.save(commit=False)
+          post.question_id = Question.objects.get(slug__iexact=slug)
+          post.save()
+          return redirect('question-details-page', slug=slug)
+
+class SignUpView(CreateView):
+  form_class = UserCreationForm
+  success_url = reverse_lazy('login')
+  template_name = 'registration/signup.html'
+  
+class QuestionCreateView(CreateView):
+  template = 'new_question.html'
+  form_class = QuestionForm
+  success_url = '/' 
+
+  def get(self, request):
+    form = QuestionForm()
+    return render(request, 'new_question.html', {'form': form})
+  
+  def post(self, request):
+    if request.method == 'POST':
+      form = QuestionForm(request.POST)
+      if form.is_valid():
+        log = form.save()
+        return HttpResponseRedirect(reverse_lazy('question-details-page', args=[log.slug]))
+      return render(request, 'new_question.html', {'form': form})
