fd44c61b0b3c0386a71c65276130eb4a0add3d89
squeaky1273 <pjriddell22@gmail.com>
2020-01-09 16:50:58 -0800

    removed line

--- forum/settings.py
+++ forum/settings.py
@@ -48,8 +48,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'forum.urls'
