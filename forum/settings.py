ee08b1a2244b5a13a231b737b1b792db59ef7595
squeaky1273 <pjriddell22@gmail.com>
2020-01-09 17:02:06 -0800

    another line

--- forum/settings.py
+++ forum/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
