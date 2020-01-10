f55481246b8739434cf2b1ae1abd65ce3572b503
squeaky1273 <pjriddell22@gmail.com>
2020-01-09 19:27:47 -0800

    try something

--- forum/settings.py
+++ forum/settings.py
@@ -15,6 +15,19 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+if bool(os.getenv('IS_PROD', False)):
+    STATICFILE_DIRECTORY = 'static'
+
+else:
+    STATICFILE_DIRECTORY = 'static/assets'
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, STATICFILE_DIRECTORY)
+]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -117,15 +130,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # playlist app settings
 QUESTION_NAME_MAX_LENGTH = 1000
